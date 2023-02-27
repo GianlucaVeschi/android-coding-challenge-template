9f3488053a3288d04a375211ea4b9d7fe31918f6
Gianluca Veschi <sheckleeer@gmail.com>
2023-02-27 16:02:52 +0100

    Make project runnable

---
+++ app/src/main/java/com/gianlucaveschi/kotlin/template/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.gianlucaveschi.kotlin.template.app
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity()
--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -1,7 +1,7 @@
-const val PUBLISHING_GROUP = "com.ncorti.kotlin.template"
+const val PUBLISHING_GROUP = "com.gianlucaveschi.kotlin.template"
 
 object AppCoordinates {
-    const val APP_ID = "com.ncorti.kotlin.template.app"
+    const val APP_ID = "com.gianlucaveschi.kotlin.template.app"
 
     const val APP_VERSION_NAME = "1.0.0"
     const val APP_VERSION_CODE = 1
